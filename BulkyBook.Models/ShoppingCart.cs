bf173cbbdb8d6d9f3710f1c97814dbc9a230644a
Vatnax <krzwojtal@gmail.com>
2023-04-26 16:15:45 +0200

    Creation

--- BulkyBook.Models/ShoppingCart.cs
+++ BulkyBook.Models/ShoppingCart.cs
@@ -27,6 +27,6 @@ namespace BulkyBook.Models
         public ApplicationUser ApplicationUser { get; set; }
 
         [NotMapped]
-        public double Price { get; set;  }
+        public double Price { get; set; }
     }
 }
